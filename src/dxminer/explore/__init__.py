9b57912f18a6d5b971a3a2a7ed03c089bfc24255
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-09-15 18:34:17 +0100

    Update explore __init__.py module

--- src/dxminer/explore/__init__.py
+++ src/dxminer/explore/__init__.py
@@ -1,21 +1,23 @@
-from .info import data_info, head_tail, ntop
-from .report import (
-    report_missingness,
-    report_uniqueness,
-    report_duplicate_rows,
-    report_duplicate_cols,
-    report_categoricals,
-    formatted_report_data_profile
-)
+from .info import data_info
+from .info import head_tail
+from .info import ntop
+from .multiple_data import data_heads
+from .report import formatted_report_data_profile
+from .report import report_categoricals
+from .report import report_duplicate_cols
+from .report import report_duplicate_rows
+from .report import report_missingness
+from .report import report_uniqueness
 
 __all__ = [
-    "data_info",
-    "head_tail",
-    "ntop",
-    "report_missingness",
-    "report_categoricals",
-    "report_duplicate_rows",
-    "report_uniqueness",
-    "report_duplicate_cols",
-    "formatted_report_data_profile"
-]
+		"data_info",
+		"head_tail",
+		"ntop",
+		"report_missingness",
+		"report_categoricals",
+		"report_duplicate_rows",
+		"report_uniqueness",
+		"report_duplicate_cols",
+		"formatted_report_data_profile",
+		"data_heads",
+		]
