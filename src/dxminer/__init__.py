cddf2007d79833965d03b21388b647ee6ee616e3
Dr.Saad.L <56449463+dr-saad-la@users.noreply.github.com>
2024-09-20 00:13:14 +0100

    Update main __init__.py configure polars to display full columns without truncation

--- src/dxminer/__init__.py
+++ src/dxminer/__init__.py
@@ -1,5 +1,12 @@
-"""Top-level package for DXMiner."""
+"""
+Top-level package for DXMiner.
+"""
 
-__author__ = """Dr Saad Laouadi"""
-__email__ = 'dr.saad.laouadi@gmail.com'
-__version__ = '0.1.0'
+__author__ = "Dr Saad Laouadi"
+__email__ = "dr.saad.laouadi@gmail.com"
+__version__ = "0.1.0"
+
+import polars as pl
+
+# Configure Polars to display all columns without truncation
+pl.Config.set_tbl_cols(-1)
